03702d278b96114dfb8495645eccbd9ef7c60c80
ReisenCW <814568436@qq.com>
2025-08-15 21:49:18 +0800

    add CollaborationContext.cs, add StaffController(not completed)

---
+++ oracle-backend/oracle-backend/Controllers/StaffController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using oracle_backend.Dbcontexts;
+using oracle_backend.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace oracle_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase{
+      
+    }
+
+}
